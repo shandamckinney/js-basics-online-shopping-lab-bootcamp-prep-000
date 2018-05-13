28a31f19b9d1f26085c4bfe3d9b542f52f3f712b
shandamckinney <mckinney.shanda@gmail.com>
2018-05-13 19:51:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -26,8 +26,8 @@ function viewCart() {
   } else if (cart.length === 2) {
     var printTwoItems = `In your cart, you have ${Object.keys(cart[cart.length-2])} at $${cart[cart.length-2][Object.keys(cart[cart.length-2])]} and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
     console.log(printTwoItems)
-  
-    
+
+
   } else {
       for (var i = 0; i < cart.length-1; i++) {
       cartItems.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`)
@@ -42,12 +42,12 @@ viewCart(cart)
 function total() {
   var pricePerItemInCart = [];
   var totalPrice = 0
-  
+
   for(var i = 0; i < cart.length; i++) {
   var itemPrice = cart[i][Object.keys(cart[i])]
     pricePerItemInCart.push(itemPrice)
     }
-  
+
   for (var j =0; j<pricePerItemInCart.length; j++) {
     totalPrice = pricePerItemInCart[j]+=totalPrice
   }
@@ -60,23 +60,23 @@ function removeFromCart(item) {
     if (cart[i].hasOwnProperty(item)) {
       cart.splice(i, 1)
       return cart
-    } 
+    }
   }
   console.log('That item is not in your cart.')
   return cart
 }
 
 removeFromCart('skis')
-  
-  
-  
+
+
+
 function placeOrder(cardNumber) {
   if(cardNumber) {
     console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
     for (var i = cart.length; i>= 0; i--) {
       cart.pop();
-    } 
-    } else 
+    }
+    } else
       console.log('Sorry, we don\'t have a credit card on file for you.')
 return cart;
   }
