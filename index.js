df19db752c4da83d95b2cb386817562f8dfa758c
shandamckinney <mckinney.shanda@gmail.com>
2018-05-13 20:00:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+  var price = Math.floor(Math.random()*99)
+  cart.push(new Object({[item]:price}))
+  console.log(`${item} has been added to your cart.`)
 }
 
 function viewCart() {
