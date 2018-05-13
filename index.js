798986cae0663e261a3e50960ed0d5ade9d97649
shandamckinney <mckinney.shanda@gmail.com>
2018-05-13 19:54:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,88 +1,78 @@
 var cart = [];
 
-function setCart(newCart) {
-   cart = newCart;
- }
+function getCart() {
+ return cart;
+}
 
-function addToCart (item) {
-  var price = Math.floor(100*(Math.random()));
-  var itemObj = {[item]: price};
-  cart.push(itemObj);
-  console.log(`${item} has been added to your cart.`);
+function setCart(c) {
+  cart = c;
   return cart;
 }
 
-
-addToCart('glove')
-addToCart('ball')
-addToCart('skis')
-addToCart('shoes')
+function addToCart(item) {
+ // write your code here
+ cart.push({[item]: Math.floor(Math.random()*100)});
+ console.log(`${item} has been added to your cart.`);
+ return cart;
+}
 
 function viewCart() {
-  var cartItems = [];
+  // write your code here
   if (cart.length === 0) {
-    console.log('Your shopping cart is empty.')
-
-  } else if (cart.length === 1) {
-    var printOneItem = `In your cart, you have ${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`
-    console.log(printOneItem)
-
-  } else if (cart.length === 2) {
-    var printTwoItems = `In your cart, you have ${Object.keys(cart[cart.length-2])} at $${cart[cart.length-2][Object.keys(cart[cart.length-2])]} and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
-    console.log(printTwoItems)
-
-
-  } else {
-      for (var i = 0; i < cart.length-1; i++) {
-      cartItems.push(` ${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}`)
-          }
-          var printThreeOrMoreItems = `In your cart, you have${cartItems}, and ${Object.keys(cart[cart.length-1])} at $${cart[cart.length-1][Object.keys(cart[cart.length-1])]}.`
-          console.log(printThreeOrMoreItems)
+    return console.log("Your shopping cart is empty.");
   }
-}
-viewCart(cart)
 
+  var string = "In your cart, you have ";
+
+  if (cart.length === 1) {
+    string += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]}.`
+  } else if (cart.length === 2) {
+    string += `${Object.keys(cart[0])} at $${cart[0][Object.keys(cart[0])]} and ${Object.keys(cart[1])} at $${cart[1][Object.keys(cart[1])]}.`
+  } else {
+    for (var i=0; i<cart.length; i++) {
+      var text;
+      if (i === cart.length - 2){
+          text = ", and ";
+      } else if (i === cart.length - 1) {
+          text = "";
+      } else {
+          text = ", ";
+      }
+      string += `${Object.keys(cart[i])} at $${cart[i][Object.keys(cart[i])]}` + text + `${i === cart.length - 1 ? '.' : ''}`;
+    }
+  }
+
+  return console.log(string);
+}
 
 function total() {
-  var pricePerItemInCart = [];
-  var totalPrice = 0
-
-  for(var i = 0; i < cart.length; i++) {
-  var itemPrice = cart[i][Object.keys(cart[i])]
-    pricePerItemInCart.push(itemPrice)
-    }
-
-  for (var j =0; j<pricePerItemInCart.length; j++) {
-    totalPrice = pricePerItemInCart[j]+=totalPrice
+  // write your code here
+  var sum = 0;
+  for (var i=0; i<cart.length; i++) {
+    sum += cart[i][Object.keys(cart[i])];
   }
-  return totalPrice
+  return sum;
 }
-total(cart)
 
 function removeFromCart(item) {
-  for (var i = 0; i <cart.length; i++) {
-    if (cart[i].hasOwnProperty(item)) {
-      cart.splice(i, 1)
-      return cart
+  // write your code here
+  for (var i=0; i<cart.length; i++) {
+    if (Object.keys(cart[i]) == item) {
+      cart = [...cart.slice(0,i), ...cart.slice(i+1)];
+      return cart;
     }
   }
-  console.log('That item is not in your cart.')
-  return cart
+  console.log("That item is not in your cart.");
+  return cart;
 }
 
-removeFromCart('skis')
-
-
-
 function placeOrder(cardNumber) {
-  if(cardNumber) {
-    console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
-    for (var i = cart.length; i>= 0; i--) {
-      cart.pop();
-    }
-    } else
-      console.log('Sorry, we don\'t have a credit card on file for you.')
-return cart;
+  // write your code here
+  if (cardNumber == null) {
+    return console.log("Sorry, we don't have a credit card on file for you.")
   }
 
-placeOrder()
+  console.log(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+  cart = [];
+  return
+}
